0fe2872400879d2358475bec67e4b5e421e6686d
Diego Gajardo <deskodoo@a2group.cl>
2024-10-25 13:41:51 -0300

    dd

--- models/stock_inventory_report_wizard.py
+++ models/stock_inventory_report_wizard.py
@@ -21,12 +21,12 @@ class StockInventoryReportWizard(models.TransientModel):
             for move_line in move.move_line_ids:
                 # Calcular el valor total basado en la cantidad y el valor unitario
                 unit_value = move.product_id.standard_price
-                total_value = move_line.qty_done * unit_value  # Usar qty_done en lugar de product_uom_qty
+                total_value = move_line.quantity * unit_value  # Usar quantity en lugar de qty_done
                 
                 stock_inventory_report.create({
                     'product_id': move.product_id.id,
                     'location_id': move.location_id.id,
-                    'quantity': move_line.qty_done,  # Usar qty_done para la cantidad movida
+                    'quantity': move_line.quantity,  # Usar quantity para la cantidad movida
                     'lot_id': move_line.lot_id.id if move_line.lot_id else False,  # El lote se toma de stock.move.line
                     'date': move.date,
                     'move_type': move.picking_type_id.name or move.reference,  # Asignar tipo de movimiento
