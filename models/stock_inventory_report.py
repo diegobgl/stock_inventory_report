727f0010f47b3a812d2e47843499c0397d8f892a
Diego Gajardo <deskodoo@a2group.cl>
2024-10-27 23:36:29 -0300

    dd

--- models/stock_inventory_report.py
+++ models/stock_inventory_report.py
@@ -10,7 +10,7 @@ class StockInventoryDateReport(models.Model):
     unit_value = fields.Float(string="Valor Unitario", required=True)  # Precio promedio ponderado
     total_value = fields.Float(string="Valor Total", compute='_compute_total_value', store=True)
 
-    @api.depends('quantity', 'unit_value')
+    #@api.depends('quantity', 'unit_value')
     def _compute_total_value(self):
         for record in self:
             record.total_value = record.quantity * record.unit_value
